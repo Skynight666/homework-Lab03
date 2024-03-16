00c2b4307d35e5b0c9df3249f58f251aa18893fc
Skynight666 <161405183+Skynight666@users.noreply.github.com>
2024-03-17 00:31:09 +0800

    Add files via upload

---
+++ JPA301.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class JPA301 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input:");
+        int n = scanner.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        System.out.println("1 + ... + " + n + " = " + sum);
+        scanner.close();
+    }
+}
